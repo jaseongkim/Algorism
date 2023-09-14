47ce05ec17cd7b9cc07bdfad2bab2bf13bf56b1e
jaseongkim <109050392+jaseongkim@users.noreply.github.com>
2023-09-14 14:03:12 +0900

    [unrated] Title: 카운트 업, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int[] answer = new int[(end_num-start_num)+1];
+        
+        int num = 0;
+        
+        for(int i = start_num; i<=end_num; i++, num++) {
+            answer[num] = i;
+        }
+        
+        
+        return answer;
+    }
+}
