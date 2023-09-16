76dbc9020833bccea2d401ce1897c9f13855a179
jaseongkim <109050392+jaseongkim@users.noreply.github.com>
2023-09-17 02:32:02 +0900

    [unrated] Title: 원소들의 곱과 합, Time: 0.02 ms, Memory: 72.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        int sum = num_list[0];
+        int mul = num_list[0];
+        
+        for(int i = 1; i< num_list.length; i++){
+            sum += num_list[i];
+            mul *= num_list[i];
+        }
+        
+        if(mul < (sum*sum)){
+            answer = 1;
+        } else {
+            answer = 0;
+        }
+        
+        
+        return answer;
+    }
+}
