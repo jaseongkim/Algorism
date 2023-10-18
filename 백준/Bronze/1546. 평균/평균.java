be8abaf25356349c1ca8a5b62fad51971c5b1690
jaseongkim <109050392+jaseongkim@users.noreply.github.com>
2023-10-19 01:00:41 +0900

    [Bronze I] Title: 평균, Time: 272 ms, Memory: 19228 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,32 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+
+		int[] point = new int[N];
+
+		long max = 0;
+		long sum = 0;
+	
+		for (int i = 0; i < N; i++) {
+			point[i] = sc.nextInt();
+			if (point[i] > max) {
+				max = point[i];
+			}
+			sum += point[i];
+		}
+
+		for (int i = 0; i < N; i++) {
+			
+		}
+
+		System.out.println(sum * 100.0 / max / N);
+
+	}
+}
