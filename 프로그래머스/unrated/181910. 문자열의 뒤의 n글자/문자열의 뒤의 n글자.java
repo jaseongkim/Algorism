b813b9dbf02f1e88a3b47d81c59e13b5cdda70a8
jaseongkim <109050392+jaseongkim@users.noreply.github.com>
2023-10-21 23:38:07 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 13.12 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String my_string, int n) {
+        String answer = "";
+        
+        int startNum = my_string.length() - n;
+        
+        for(int i  = startNum; i<my_string.length(); i++){
+            answer += my_string.charAt(i);
+        }
+        
+        
+        
+        return answer;
+    }
+}
