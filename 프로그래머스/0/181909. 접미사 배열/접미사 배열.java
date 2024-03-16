4195ffadedb7e83bfdbdd85df0abed8c0c8183fa
jaseongkim <109050392+jaseongkim@users.noreply.github.com>
2024-03-16 16:38:56 +0900

    [level 0] Title: 접미사 배열, Time: 0.17 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        
+        answer = new String[my_string.length()];
+        
+        for(int i = 0; i<answer.length; i++) {
+            answer[i] = my_string.substring(i);
+        }
+        
+        Arrays.sort(answer);
+        
+       
+        
+        return answer;
+    }
+}
